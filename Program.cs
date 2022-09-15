98324f0a534147ff0817c5ce724feda90ec18c77
Renan Lima <renan.limagomes@gmail.com>
2022-09-15 14:38:40 -0300

    Correção do CORS

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       builder =>
                       {
-                          builder.WithOrigins("http://localhost:4200");
+                          builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
                       });
 });
 
