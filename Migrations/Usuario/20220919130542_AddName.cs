807df0a61b9d6ea2d090624576cb928eea02c27e
Renan Lima <renan.limagomes@gmail.com>
2022-09-19 10:06:35 -0300

    Inserir atributo Name em Usuario

---
+++ Migrations/Usuario/20220919130542_AddName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetShop_BackEnd.Migrations.Usuario
+{
+    public partial class AddName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "Usuarios",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "Usuarios");
+        }
+    }
+}
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -3,6 +3,8 @@ namespace PetShop_BackEnd.Models
     public class Usuario
 {
     public long id { get; set; }
+
+    public string? name {get; set;}
     public string? username { get; set; }
     public string? password { get; set; }
 
